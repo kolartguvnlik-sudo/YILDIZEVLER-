31029da6e473535e24b592a7e8de7d5834ddde37
kolartguvnlik-sudo <kolartguvnlik@gmail.com>
2026-02-06 05:29:31 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDC0WMcPeZwJ5laJttALR-3Ww_WbpIfKVU",
+  authDomain: "yildizkule-82f2f.firebaseapp.com",
+  databaseURL: "https://yildizkule-82f2f-default-rtdb.firebaseio.com",
+  projectId: "yildizkule-82f2f",
+  storageBucket: "yildizkule-82f2f.firebasestorage.app",
+  messagingSenderId: "494296409424",
+  appId: "1:494296409424:web:c1eb063e7257f256fff855",
+  measurementId: "G-99EHBNTJWQ"
+};
